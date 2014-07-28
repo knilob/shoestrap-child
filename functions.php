a7fd883a1c10400f0f0eed9335a88b0828b46a39
Bo Link <knilob@gmail.com>
2014-07-27 23:27:44 -0500

    Removed auto compiling of VU LESS styles.

--- functions.php
+++ functions.php
@@ -7,18 +7,18 @@ if ( !defined( 'SHOESTRAP_OPT_NAME' ) )
 #require_once locate_template( 'lib/vu-banner-options.php' );
 require_once locate_template( 'lib/vu-brand-options.php' );
 //load overrides
-require_once locate_template('lib/overrides/sidebar.php');
-require_once locate_template('lib/overrides/footer.php');
+require_once locate_template( 'lib/overrides/sidebar.php' );
+require_once locate_template( 'lib/overrides/footer.php' );
 
 /*
  * Add a less file from our child theme to the parent theme's compiler.
  * This uses the 'shoestrap_compiler' filter that exists in the shoestrap compiler
  */
-add_filter( 'shoestrap_compiler', 'shoestrap_child_styles' );
-function shoestrap_child_styles( $bootstrap ) {
-  return $bootstrap . '
-  @import "' . get_stylesheet_directory() . '/assets/less/child.less";';
-}
+// add_filter( 'shoestrap_compiler', 'shoestrap_child_styles' );
+// function shoestrap_child_styles( $bootstrap ) {
+//   return $bootstrap . '
+//   @import "' . get_stylesheet_directory() . '/assets/less/style.less";';
+// }
 
 /*
  * Changes the output of the compiled CSS.
@@ -39,7 +39,7 @@ function shoestrap_child_hijack_compiler( $css ) {
  */
 // Uncomment the line below to enqueue the stylesheet
 // Use a priority greater than 100 to enqueue it after the main stylesheet
-add_action('wp_enqueue_scripts', 'shoestrap_child_load_stylesheet', 103);
+add_action('wp_enqueue_scripts', 'shoestrap_child_load_stylesheet', 101);
 function shoestrap_child_load_stylesheet() {
   wp_enqueue_style( 'shoestrap_child_css', get_stylesheet_uri(), false, null );
 }
@@ -47,12 +47,12 @@ function shoestrap_child_load_stylesheet() {
 /*
  * Enqueue the generated LESS styles locally for testing
  */
-if ( strpos( $_SERVER['DOCUMENT_ROOT'], 'linkja' ) )  {
-  add_action('wp_enqueue_scripts', 'shoestrap_child_load_local_less', 104);
-  function shoestrap_child_load_local_less() {
-    wp_enqueue_style( 'shoestrap_local_less', get_stylesheet_directory_uri() . '/assets/css/child.css', false, null );
-  }
-}
+// if ( strpos( $_SERVER['DOCUMENT_ROOT'], 'linkja' ) )  {
+//   add_action('wp_enqueue_scripts', 'shoestrap_child_load_local_less', 104);
+//   function shoestrap_child_load_local_less() {
+//     wp_enqueue_style( 'shoestrap_local_less', get_stylesheet_directory_uri() . '/assets/css/style.css', false, null );
+//   }
+// }
 
 /* Override templates/top-bar */
 function vu_override_top_bar_template() {
@@ -77,7 +77,7 @@ function shoestrap_load_slider() {
     function shoestrap_slider_styles() {
       wp_enqueue_style( 'shoestrap_slider_css', get_stylesheet_directory_uri() . '/assets/bower_components/slick-carousel/slick/slick.css', false, null );
     }
-    add_action( 'wp_enqueue_scripts', 'shoestrap_slider_styles', 102 );
+    add_action( 'wp_enqueue_scripts', 'shoestrap_slider_styles', 100 ) ;
 
     /**
      * Our Slick Carousel script
