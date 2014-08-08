71d02bb4302e30b6f3b4bd137845cc985f9ae5c8
Bo Link <knilob@gmail.com>
2014-08-08 13:55:56 -0500

    Added custom field shortcode.

--- functions.php
+++ functions.php
@@ -138,7 +138,6 @@ function field_func($atts) {
    global $post;
    $name = $atts['name'];
    if (empty($name)) return;
-
    return get_post_meta($post->ID, $name, true);
 }
 add_shortcode('field', 'field_func');
