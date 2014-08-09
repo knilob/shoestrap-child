4280b622f33495c1b842887047158c169085c63a
Bo Link <knilob@gmail.com>
2014-08-09 00:08:26 -0500

    Working out JS toggling.

--- functions.php
+++ functions.php
@@ -164,7 +164,7 @@ function parse_shortcode_content( $content ) {
 
 // accordion
 function accordion_open_tag(  $atts, $content = null ) {
-  wp_enqueue_script('accordion-script', get_stylesheet_directory_uri() . '/assets/js/min/accordion-min.js', array('jquery'), '1.0', true);
+  wp_enqueue_script('accordion-script', get_stylesheet_directory_uri() . '/assets/js/accordion.js', array('jquery'), '1.0', true);
   $content = parse_shortcode_content( $content );
 
   return "<link rel='stylesheet' type='text/css' href='" . get_stylesheet_directory_uri() . "/assets/css/accordion.css' media='screen' /><ul class='accordion collapsible'>".do_shortcode($content)."</ul>";
