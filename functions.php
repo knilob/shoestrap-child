a35ade68305b9511d395474e30ccefe5f561b999
Bo Link <knilob@gmail.com>
2014-08-08 23:08:55 -0500

    Stupid error in style queue.

--- functions.php
+++ functions.php
@@ -165,7 +165,7 @@ function parse_shortcode_content( $content ) {
 // accordion
 function accordion_open_tag(  $atts, $content = null ) {
   wp_enqueue_script('accordion-script', get_stylesheet_directory_uri() . '/assets/js/min/accordion-min.js', array('jquery'), '1.0', true);
-  wp_enqueue_style('accordion-css', get_stylesheet_directory_uri() . '/assets/css/accordion.css', array(''), '1.0', all);
+  wp_enqueue_style('accordion-css', get_stylesheet_directory_uri() . '/assets/css/accordion.css', array(''), '1.0', 'all');
   $content = parse_shortcode_content( $content );
 
   return "<ul class='accordion collapsible'>".do_shortcode($content)."</ul>";
