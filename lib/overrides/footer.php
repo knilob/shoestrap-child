377af1ff996c2c58d43e0d74ba490feed6603d45
Bo Link <knilob@gmail.com>
2015-05-08 23:18:43 -0500

    Updated footer wp-login link.

--- lib/overrides/footer.php
+++ lib/overrides/footer.php
@@ -13,7 +13,7 @@ function my_custom_footer_override() {
           <?php shoestrap_footer_social_links(); ?>
         </div>
         <p>
-          <span><a href="http://www.vanderbilt.edu/wp-redirect<?php global $current_blog; $blog_path = $current_blog->path; echo $blog_path; ?>">©</a></span>
+          <span><a href="<?php echo wp_login_url(); ?>">©</a></span>
           <script type="text/javascript">date=new Date(); year=date.getFullYear(); document.write(year);</script>&nbsp;Vanderbilt University · <br><a href="http://web.vanderbilt.edu/">Site Development: University Web Communications</a>
         </p>
       </div>
